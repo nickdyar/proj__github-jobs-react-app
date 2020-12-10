e28d763425b38bfee827fccb03d76866cccf26fd
nickdyar <nick.dyar@gmail.com>
2020-12-10 03:10:34 -0500

    Create custom hook useFetchJobs to interface with API.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import useFectchJobs from './useFetchJobs';
+import { Container } from 'react'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+
+  const { jobs, loading, error } = useFetchJobs()
+
+  return (
+    <Container>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error -- Refresh page.</h1>}
+      <h1>{jobs.length}</h1>
+    </Container>
+
+  )
+}
+
+export default App;
--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,11 +1,10 @@
 import { useReducer } from 'react';
 
-function reducer(state, action) {
-
-}
+function reducer(state, action) {}
 
 export default function useFetchJobs(params, page) {
 
+// eslint-disable-next-line
   const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true })
 
   dispatch({ type: 'hello', payload: { x: 3}})
