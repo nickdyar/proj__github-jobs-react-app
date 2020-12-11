d829d3a3854ee35b4ec1217b5b501ed7d079c542
nickdyar <nick.dyar@gmail.com>
2020-12-11 05:20:07 -0500

    Install and configure eslint, prettier, and stylelint.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import useFetchJobs from './useFetchJobs';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
+import useFetchJobs from './useFetchJobs';
+import Job from './Job';
+import JobsPagination from './JobsPagination';
 
 function App() {
-
-  const { jobs, loading, error } = useFetchJobs()
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+  const { jobs, loading, error } = useFetchJobs(params, page);
 
   return (
-    <Container>
-      {loading && <h1>Loading...</h1>},
-      {error && <h1>Error. Refresh page.</h1>},
-      <h1>{jobs.length}</h1>
+    <Container className='my-4'>
+      <h1 className='mb-4'>Github Jobs</h1>
+      <JobsPagination page={page} setPage={setPage} />
+      {loading && <h1>Fetching jobs...</h1>}
+      {error && <h1>Oops! Try refreshing browser.</h1>}
+      {jobs.map(job => (
+        <Job key={job.id} job={job} />
+      ))}
+      <JobsPagination page={page} setPage={setPage} hasNextPage />
     </Container>
   );
 }
--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -5,46 +5,53 @@ const ACTIONS = {
   MAKE_REQUEST: 'make-request',
   GET_DATA: 'get-data',
   ERROR: 'error',
-}
+};
 
-const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
+const BASE_URL =
+  'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json';
 
 function reducer(state, action) {
   switch (action.type) {
     case ACTIONS.MAKE_REQUEST:
-      return { loading: true, jobs: [] } // clears jobs on each new request
+      return { loading: true, jobs: [] }; // clears jobs on each new request
     case ACTIONS.GET_DATA:
-      return { ...state, loading: false, jobs: action.payload.jobs }
+      return { ...state, loading: false, jobs: action.payload.jobs };
     case ACTIONS.ERROR:
-      return { ...state, loading: false, error: action.payload.error, jobs: [] }
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        jobs: [],
+      };
     default:
-      return state
+      return state;
   }
 }
 
 export default function useFetchJobs(params, page) {
   // make hook do something => useReducer will make it do somethin'
-  const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true })
+  const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true });
 
   useEffect(() => {
-    const cancelToken = axios.CancelToken.source()
-    dispatch({ type: ACTIONS.MAKE_REQUEST })
-    axios.get(BASE_URL, {
-      cancelToken: cancelToken.token,
-      params: { markdown: true, page: page, ...params }
-    }).then(res => {
-      dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data} })
-    }).catch(e => {
-      if (axios.isCancel(e)) return
-      dispatch({ type: ACTIONS.ERROR, payload: { error: e } })
-    })
+    const cancelToken = axios.CancelToken.source();
+    dispatch({ type: ACTIONS.MAKE_REQUEST });
+    axios
+      .get(BASE_URL, {
+        cancelToken: cancelToken.token,
+        params: { markdown: true, page, ...params },
+      })
+      .then(res => {
+        dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } });
+      })
+      .catch(e => {
+        if (axios.isCancel(e)) return;
+        dispatch({ type: ACTIONS.ERROR, payload: { error: e } });
+      });
 
     return () => {
-      cancelToken.cancel()
-    }
+      cancelToken.cancel();
+    };
+  }, [params, page]);
 
-  }, [params, page])
-
-
-  return state
+  return state;
 }
