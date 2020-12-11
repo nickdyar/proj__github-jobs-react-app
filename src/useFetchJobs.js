7662921c34572ba00d9748e25436a9f8075437a6
nickdyar <nick.dyar@gmail.com>
2020-12-11 03:23:13 -0500

    Create Job component.

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,17 +1,50 @@
-import { useReducer } from 'react';
+import { useReducer, useEffect } from 'react';
+import axios from 'axios';
 
-function reducer(state, action) {}
+const ACTIONS = {
+  MAKE_REQUEST: 'make-request',
+  GET_DATA: 'get-data',
+  ERROR: 'error',
+}
+
+const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.MAKE_REQUEST:
+      return { loading: true, jobs: [] } // clears jobs on each new request
+    case ACTIONS.GET_DATA:
+      return { ...state, loading: false, jobs: action.payload.jobs }
+    case ACTIONS.ERROR:
+      return { ...state, loading: false, error: action.payload.error, jobs: [] }
+    default:
+      return state
+  }
+}
 
 export default function useFetchJobs(params, page) {
-
-// eslint-disable-next-line
+  // make hook do something => useReducer will make it do somethin'
   const [state, dispatch] = useReducer(reducer, { jobs: [], loading: true })
 
-  dispatch({ type: 'hello', payload: { x: 3}})
+  useEffect(() => {
+    const cancelToken = axios.CancelToken.source()
+    dispatch({ type: ACTIONS.MAKE_REQUEST })
+    axios.get(BASE_URL, {
+      cancelToken: cancelToken.token,
+      params: { markdown: true, page: page, ...params }
+    }).then(res => {
+      dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data} })
+    }).catch(e => {
+      if (axios.isCancel(e)) return
+      dispatch({ type: ACTIONS.ERROR, payload: { error: e } })
+    })
 
-  return {
-    jobs: [],
-    loading: false,
-    error: true
-  }
+    return () => {
+      cancelToken.cancel()
+    }
+
+  }, [params, page])
+
+
+  return state
 }
